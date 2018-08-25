f47f664c9c1d728e7770440988ebbeffef551812
Runaljod <skahkovde@mail.ru>
2018-08-25 19:49:33 +0300

    накидал страницу удаления токена

--- src/pages/pin/pin.ts
+++ src/pages/pin/pin.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { SendPage } from '../send/send';
+import { DeletePage } from '../delete/delete';
 
 /**
  * Generated class for the PinPage page.
@@ -18,7 +19,9 @@ export class PinPage {
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
-
+  openDelete() {
+    this.navCtrl.push(DeletePage);
+  }
   ionViewDidLoad() {
     console.log('ionViewDidLoad PinPage');
   }
