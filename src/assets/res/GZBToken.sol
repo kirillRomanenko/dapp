f10699a84c5393eeeb5085acf0d8b01983dd41d0
kirillRomanenko <romanenko.kirill1995@gmail.com>
2018-08-28 00:57:29 +0300

    fix contract

--- src/assets/res/GZBToken.sol
+++ src/assets/res/GZBToken.sol
@@ -1,7 +1,7 @@
 pragma solidity ^0.4.18;
 
 contract GZBTOKEN {
-    /* This creates an array with all balances */
+    
     mapping (address => uint256) public balanceOf;
 
     string public constant name = "GAZPROMBANK";
@@ -10,12 +10,12 @@ contract GZBTOKEN {
 
     function GZBTOKEN() public { balanceOf[msg.sender] = 1000000;}
 
-    /* Send coins */
+    // отправка токенов 
    function transfer(address _to, uint256 _value) public {
-     require(balanceOf[msg.sender] >= _value);           // Check if the sender has enough
-     require(balanceOf[_to] + _value >= balanceOf[_to]); // Check for overflows
-     balanceOf[msg.sender] -= _value;                    // Subtract from the sender
-     balanceOf[_to] += _value;                          // Add the same to the recipient
+     require(balanceOf[msg.sender] >= _value);           
+     require(balanceOf[_to] + _value >= balanceOf[_to]); 
+     balanceOf[msg.sender] -= _value;                    
+     balanceOf[_to] += _value;                          
 }
 }
 
